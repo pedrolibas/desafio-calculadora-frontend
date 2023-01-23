14049b8406715801c6f19a4eba84c5494d7c3272
Pedro Gabriel <pedrogv03@hotmail.com>
2023-01-22 23:40:03 -0300

    :rocket: finished logic and functionality

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import CalculatorProvider from "./contexts/CalculatorContext";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <CalculatorProvider>
+      <App />
+    </CalculatorProvider>
   </React.StrictMode>
 );
